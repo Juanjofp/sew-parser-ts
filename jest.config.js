b823304aa3e87a4b3fb007198637c53869f2867e
Juanjo <juanjo@juanjofp.com>
2021-01-04 15:42:36 +0100

    Prepare package

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,12 @@ module.exports = {
     collectCoverage: true,
     collectCoverageFrom: ['src/**/*.ts', '!src/index.ts'],
     coverageThreshold: {
-        statements: 100,
-        branches: 100,
-        functions: 100,
-        lines: 100
+        global: {
+            statements: 100,
+            branches: 100,
+            functions: 100,
+            lines: 100
+        }
     },
     watchPlugins: [
         'jest-watch-typeahead/filename',
